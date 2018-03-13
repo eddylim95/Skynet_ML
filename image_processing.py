c9a63952f80a5c55f0820444f6add261355c749d
eddylim95 <eddylim95@hotmail.com>
2018-03-13 20:07:34 +0800

    working

--- image_processing.py
+++ image_processing.py
@@ -26,13 +26,16 @@ folders = [
 my_path = os.path.abspath(os.path.dirname(__file__))
 
 for name in folders:
-	i = 1
+	i = 2
 	while (True):
 		try:
 			filename = os.path.join(my_path, (name + "\\" + name + "_" + str(i) + ".jpg"))
 			image = cv2.imread(filename, 0)
 			resized_image = cv2.resize(image, (150, 150)) 
-			filename = filename.replace('.jpg', '_grey.jpg')
+			filename = os.path.join(my_path, ("grey" + "\\" + name + "\\"))
+			if not os.path.exists(filename):
+				os.makedirs(filename)
+			filename = os.path.join(filename + name + "_" + str(i) + "_grey.jpg")
 			cv2.imwrite(filename, resized_image)
 			i += 1
 		except:
