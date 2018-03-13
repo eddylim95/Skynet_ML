11d13af6632dcfc0fdf82b15112a8c1f39be5d04
eddylim95 <eddylim95@hotmail.com>
2018-03-13 18:33:57 +0800

    problem

---
+++ image_processing.py
@@ -0,0 +1,37 @@
+import cv2
+import numpy as np
+
+folders = [ 
+    'BabyBibs',
+    'BabyHat',
+    'BabyPants',
+    'BabyShirt',
+    'PackageFart',
+    'womanshirtsleeve',
+    'womencasualshoes',
+    'womenchiffontop',
+    'womendollshoes',
+    'womenknittedtop',
+    'womenlazyshoes',
+    'womenlongsleevetop',
+    'womenpeashoes',
+    'womenplussizedtop',
+    'womenpointedflatshoes',
+    'womensleevelesstop',
+    'womenstripedtop',
+    'wrapsnslings'
+]
+
+for name in folders:
+	i = 1
+	while (True):
+		try:
+			filename = "\\" + name + "\\" + name + "_" + str(i) + ".jpg"
+			print(filename)
+			image = cv2.imread(filename, 0)
+			resized_image = cv2.resize(image, (150, 150)) 
+			cv2.imwrite(filename + "_Grey", resized_image)
+			i += 1
+		except:
+			print(name + ' error')
+			break;
