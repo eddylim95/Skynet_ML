c7a86a2a0c63589e030fa9d5668c9eaf6470bf77
eddylim95 <eddylim95@hotmail.com>
2018-03-13 19:50:59 +0800

    fixed!

--- image_processing.py
+++ image_processing.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import os
 
 folders = [ 
     'BabyBibs',
@@ -22,14 +23,17 @@ folders = [
     'wrapsnslings'
 ]
 
+my_path = os.path.abspath(os.path.dirname(__file__))
+
 for name in folders:
 	i = 1
 	while (True):
 		try:
-			filename = "\\" + name + "\\" + name + "_" + str(i)
+			filename = os.path.join(my_path, (name + "\\" + name + "_" + str(i) + ".jpg"))
 			image = cv2.imread(filename, 0)
 			resized_image = cv2.resize(image, (150, 150)) 
-			cv2.imwrite(filename + "_Grey", resized_image)
+			filename = filename.replace('.jpg', '_grey.jpg')
+			cv2.imwrite(filename, resized_image)
 			i += 1
 		except:
 			print(name + ' error')
